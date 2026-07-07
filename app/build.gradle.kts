a6f3a12d0530ca1a39636556801d99a712330d82
helloworld3003 <sarkartapomoy12nov@gmail.com>
2026-07-07 16:10:28 +0530

    feat: add F-Droid metadata and configure initial Android build settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.helloworld3003.livewallpaperdinogame"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.3.0"
+        versionCode = 4
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
